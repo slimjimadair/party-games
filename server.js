d08471a1cb3e6844c555c47c637097f9a1c74aff
slimjimadair <slimjimadair@gmail.com>
2020-12-28 18:52:14 +0000

    Single App
    Combined to single app structure - gameplay will be handled within game sections / files.

--- server.js
+++ server.js
@@ -13,3 +13,8 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // Start server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// Handle a socket connection request from web client
+io.on('connection', socket => {
+  console.log('New WS Connection')
+})
